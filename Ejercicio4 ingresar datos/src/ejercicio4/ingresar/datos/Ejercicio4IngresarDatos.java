5144b83a36f39ade71392ed95d0502b8a9ec9bba
gimenavargas <1471366@senati.pe>
2023-03-16 10:42:27 -0500

    ingresar tu nombre

--- Ejercicio4 ingresar datos/src/ejercicio4/ingresar/datos/Ejercicio4IngresarDatos.java
+++ Ejercicio4 ingresar datos/src/ejercicio4/ingresar/datos/Ejercicio4IngresarDatos.java
@@ -15,6 +15,10 @@ public class Ejercicio4IngresarDatos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ingresa tu nombre");
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("ingresa tu nombre:");
+        String cadena=entrada.nextLine();
+      
+        
     }
 }
